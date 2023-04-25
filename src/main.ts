c56cfc69df312a0c6eeb158d70bab4cdff10f2f2
ilya07092000 <ischenko2012@gmail.com>
2023-04-25 13:13:55 +0300

    added swagger to the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Music App Api')
+    .setDescription('Api Description for Music App')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs/api', app, document);
+
   await app.listen(process.env.PORT || 5000, () =>
     console.log(`Server Started On Port ${process.env.PORT || 5000}`),
   );
