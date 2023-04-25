9dbc32485147259e8e5df1dc0cc74ffe44527bdf
ilya07092000 <ischenko2012@gmail.com>
2023-04-25 13:10:35 +0300

    connected mongoose to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB)],
   controllers: [AppController],
   providers: [AppService],
 })
