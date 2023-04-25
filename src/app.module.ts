dda3dc25a455817dc15e771f91665f986c99ab8b
ilya07092000 <ischenko2012@gmail.com>
2023-04-25 13:30:02 +0300

    generated track instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB)],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB),
+    TrackModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
