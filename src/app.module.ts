9520f985eb65647d1c350b05d5a24f3dc750876f
ilya07092000 <ischenko2012@gmail.com>
2023-04-26 18:08:04 +0300

    added file module and server static to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrackModule } from './track/track.module';
+import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB),
     TrackModule,
+    FileModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'static'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
