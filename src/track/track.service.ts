a8c69efb0db16342f73e9fc6fb56db3a0cd31f3a
ilya07092000 <ischenko2012@gmail.com>
2023-04-27 15:51:51 +0300

    feat: track service

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,21 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { FileService } from 'src/file/file.service';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { Track } from './track.model';
 
 @Injectable()
 export class TrackService {
-  async create() {
-    return 'Created';
+  constructor(
+    @InjectModel(Track.name) private trackRepository: Model<Track>,
+    private fileService: FileService,
+  ) {}
+
+  async create(dto: CreateTrackDto, audio: Express.Multer.File) {
+    let audioFileName = '';
+    if (audio) {
+      audioFileName = this.fileService.save(audio);
+    }
+
+    const track = await this.trackRepository.create({
+      ...dto,
+      audio: audioFileName,
+    });
+    return track;
   }
 
   async getAll() {
-    return 'Get All';
+    return this.trackRepository.find();
   }
 
   async getById(id: string) {
-    return id;
+    const track = await this.trackRepository.findById(id);
+
+    if (!track) {
+      throw new HttpException('Track Not Found', HttpStatus.NOT_FOUND);
+    }
+
+    return track;
   }
 
-  async deleteById() {
-    return 'deleted';
+  async deleteById(id: string) {
+    const track = await this.trackRepository.findOneAndDelete({ _id: id });
+
+    if (!track) {
+      throw new HttpException('Track Not Found', HttpStatus.NOT_FOUND);
+    }
+
+    return track;
   }
 
   async update() {
