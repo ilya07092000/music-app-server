7cd16e2dcccb017342dc69dc6d75e0bd4988046e
ilya07092000 <ischenko2012@gmail.com>
2023-04-27 15:51:36 +0300

    added new imports in track module

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from 'src/file/file.module';
 import { TrackController } from './track.controller';
+import { Track, TrackSchema } from './track.model';
 import { TrackService } from './track.service';
 
 @Module({
   controllers: [TrackController],
   providers: [TrackService],
+  imports: [
+    FileModule,
+    MongooseModule.forFeature([{ name: Track.name, schema: TrackSchema }]),
+  ],
 })
 export class TrackModule {}
