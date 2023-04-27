17a5bec1daa86551160c7b4456668a299efa60a4
ilya07092000 <ischenko2012@gmail.com>
2023-04-27 15:51:22 +0300

    fix: removed unique artist from track schema

--- src/track/track.model.ts
+++ src/track/track.model.ts
@@ -8,7 +8,7 @@ export class Track {
   @Prop({ type: String, required: true, unique: true })
   name: string;
 
-  @Prop({ type: String, required: true, unique: true })
+  @Prop({ type: String, required: true, unique: false })
   artist: string;
 
   @Prop({ type: String })
